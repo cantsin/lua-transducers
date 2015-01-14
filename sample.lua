d82c4f19daf65aa5f27d058fd53507b608256682
James Tranovich <jtranovich@gmail.com>
2015-01-13 20:08:59 -0800

    Added inspect.lua.

--- sample.lua
+++ sample.lua
@@ -1,11 +1,5 @@
 require 'transducers'
 
-local function print_arr(seq)
-  for i=1,#seq do
-    io.write(seq[i], ' ')
-  end
-  print()
-end
 
 local function plus1(n)
   return n + 1
@@ -25,17 +19,17 @@ local arr = {1,2,3,4}
 -- demonstrating transduce.
 local transducer = map(plus1)
 local result = transduce(transducer, push, {}, arr)
-print_arr(result)
--- {2,3,4,5}
+print(inspect(result))
+-- { 2, 3, 4, 5 }
 
 -- demonstrating compose.
 local transducer = compose(map(plus1), map(plus1), map(plus1), map(plus1))
 local result = transduce(transducer, push, {}, arr)
-print_arr(result)
--- {5,6,7,8}
+print(inspect(result))
+-- { 5, 6, 7, 8 }
 
 -- demonstrating filter.
 local transducer = compose(filter(odd), map(plus1), map(plus1), map(plus1))
 local result = transduce(transducer, push, {}, arr)
-print_arr(result)
--- {5,7}
+print(inspect(result))
+-- { 5, 7 }
