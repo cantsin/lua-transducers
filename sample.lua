9f27248fd2d8b7f2c40b6c11c1f7b3f20f081b9d
James Tranovich <jtranovich@gmail.com>
2015-01-13 19:02:31 -0800

    More descriptions.

--- sample.lua
+++ sample.lua
@@ -1,18 +1,25 @@
 require 'transducers'
 
-local arr = {1,2,3,4}
-local push = function(tbl, index)
+local function plus1(n)
+  return n + 1
+end
+
+local function push(tbl, index)
   table.insert(tbl, index)
   return tbl
 end
-local result = transduce(map(function(x) return x + 1 end), push, {}, arr)
+
+local arr = {1,2,3,4}
+
+-- demonstrating transduce.
+local transducer = map(plus1)
+local result = transduce(transducer, push, {}, arr)
 for i=1,#result do
   print(result[i])
 end
 -- {2,3,4,5}
 
-local function plus1(n) return n + 1 end
-
+-- demonstrating compose.
 local transducer = compose(map(plus1), map(plus1), map(plus1), map(plus1))
 local result = transduce(transducer, push, {}, arr)
 for i=1,#result do
