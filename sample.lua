d8585a265f0b05509bbf6ced4e12edc69012c3ae
James Tranovich <jtranovich@gmail.com>
2015-01-13 19:00:26 -0800

    Compose example.

--- sample.lua
+++ sample.lua
@@ -10,3 +10,12 @@ for i=1,#result do
   print(result[i])
 end
 -- {2,3,4,5}
+
+local function plus1(n) return n + 1 end
+
+local transducer = compose(map(plus1), map(plus1), map(plus1), map(plus1))
+local result = transduce(transducer, push, {}, arr)
+for i=1,#result do
+  print(result[i])
+end
+-- {5,6,7,8}
